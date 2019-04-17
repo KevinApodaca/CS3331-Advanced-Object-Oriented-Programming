c47c3a7d2ab49910580c847889afc4b88d2b1902
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-04-17 11:49:30 -0600

    javadoc on the rest of the files

--- HW3/PriceWatcher/base/Main.java
+++ HW3/PriceWatcher/base/Main.java
@@ -1,3 +1,42 @@
+/**
+ * CS 3331 Advanced Object Oriented Programming
+ * @author Kevin Apodaca, Imani Martin
+ * @since 4/17/19
+ * In this assignment, you are to extend your HW2 program to support
+	multiple items. Focus on the design of the UI. You will learn and
+	become familar with several Swing widgets such as JDialog, JList (or
+	JTable), JMenu, JPopupMenu, JMenuBar and JToolBar. Your application
+	shall meet all the relevant requirements from HW2 as well as the
+	following new ones.
+
+	R1. Provide a way to manage the list of items whose prices are to be
+		watched over. The user should be able to add a new item, remove an
+		existing item, and change an existing item, e.g., rename the item
+		or change its URL (see R3, R4 and R5 below).
+
+	R2. Display all watched items along with their price changes. Consider
+		using a JList (or JTable) for this.
+
+	R3. Use custom dialogs (subclasses of JDialog) to add and change an
+		item in the watch list.
+
+	R4. Improve the user interface by proving a menu and a tool bar to (a)
+		add a new item and (b) to check the current prices of all
+		items. For each menu item, provide an icon, a mnemonic and an
+		accelerator. For each tool bar button, use an icon and provide a
+		tool tip.
+
+	R5. Provide a popup menu to manipulate an indiviual item. Your popup
+		menu shall include menu items for:
+
+		- Checking the current price
+		- Viewing its webpage
+		- Editing it (change the name and URL; see R1 above)
+		- Removing it (see R1 above)
+
+	R6. Use JavaDoc to document your classes. Write a Javadoc comment for
+		each class/interface, field, constructor and method.
+ */
 package PriceWatcher.base;
 
 import PriceWatcher.model.Item;
@@ -14,10 +53,15 @@ import java.text.DecimalFormat;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+/**
+ * Creating our JFrame using java swing.
+ */
 @SuppressWarnings("serial")
 public class Main extends JFrame {
 
-
+/**
+ * New instances of the items which we will check the price of.
+ */
     private Item[] items = {
             new Item("LED monitor", 61.13, "https://www.bestbuy.com/site/samsung-ue590-series-28-led-4k-uhd-monitor-black/5484022.p?skuId=5484022", "3/4/19"),
 
@@ -30,7 +74,7 @@ public class Main extends JFrame {
     private DefaultListModel model;
 
 
-    /** Default dimension of the dialog. */
+    /** Default dimension of the dialog box. */
     private final static Dimension DEFAULT_SIZE = new Dimension(500, 300);
 
     /** Special panel to display the watched item. */
@@ -47,10 +91,13 @@ public class Main extends JFrame {
         this(DEFAULT_SIZE);
     }
 
-    /** Create a new dialog of the given screen dimension. */
+/**
+ * Here we createa  new dialog box with the dimensions passed and we configure all settings needed for things to be displayed properly.
+ * @param dim
+ */
     public Main(Dimension dim) {
         super("PRICE WATCHER");
-
+        // new instance of the priceFinder.
         this.priceFinder = new PriceWatcher.model.PriceFinder();
 
         setSize(dim);
@@ -68,7 +115,9 @@ public class Main extends JFrame {
         public MyComponentListener(int width, int height) {
             this.width = width; this.height = height;
         }
-
+/**
+ * Here we resize our windows by getting height and width dimensinos.
+ */
         public void componentResized(ComponentEvent e) {
             Component c = e.getComponent();
             if (c.getWidth() < width || c.getHeight() < height) {
@@ -78,11 +127,10 @@ public class Main extends JFrame {
         }
     }
 
-    /**
-     * Callback to be invoked when the refresh button is clicked. Find the current
-     * price of the watched item and display it along with a percentage price
-     * change.
-     */
+/**
+ * Callback to be invoked when the refresh button is clicked by the user. Method will then find the current price of the watched item and display it along with a percentage price change.
+ * @param event
+ */
     private void refreshButtonClicked(ActionEvent event) {
         for (Item item : this.items) {
             item.updatePrice(this.priceFinder.getNewPrice(item.getURL()));
@@ -90,11 +138,10 @@ public class Main extends JFrame {
         }
         super.repaint();
     }
-
-    /**
-     * Callback to be invoked when the view-page icon is clicked. Launch a (default)
-     * web browser by supplying the URL of the item.
-     */
+/**
+ * Callback to be invoked when the view-page icon is clicked by the user. This will launch the user's default web browser and open the URL of the item chosen.
+ * @param event
+ */
     private void viewPageClicked(ActionEvent event) {
         Desktop desktop = Desktop.getDesktop();
         for (Item item : this.items) {
@@ -147,7 +194,10 @@ public class Main extends JFrame {
         add(pane, BorderLayout.CENTER);
         add(msgBar, BorderLayout.SOUTH);
     }
-
+/**
+ * Adding all menu options available to the user to a menu bar
+ * @return the newly created menu bar
+ */
     private JMenuBar buildMenuBar(){
         JMenuBar menuBar = new JMenuBar();
 
@@ -162,7 +212,13 @@ public class Main extends JFrame {
 
         return menuBar;
     }
-
+/**
+ * Here we create a toolbar that the user will interact with to do the following:
+    *  Add items
+    *  Remove an item
+    *  Edit the information of an item.
+ * @return completed toolbar
+ */
     private JToolBar createToolBar(){
         JToolBar toolbar = new JToolBar();
         toolbar.setRollover(true);
@@ -173,12 +229,13 @@ public class Main extends JFrame {
         JButton addBtn = new JButton(rescaleImage(createImageIcon("add")));
         addBtn.setToolTipText("Add an additional item");
 
-        JButton removeBtn = new JButton(rescaleImage(createImageIcon("remove.png")));
+        JButton removeBtn = new JButton(rescaleImage(createImageIcon("remove.png")));   //adding remove icon png.
         removeBtn.setToolTipText("Remove from list");
 
-        JButton editBtn = new JButton(rescaleImage(createImageIcon("edit.png")));
+        JButton editBtn = new JButton(rescaleImage(createImageIcon("edit.png")));   //adding the edit icon png
         editBtn.setToolTipText("Edit item");
 
+        /** Adding the button options that will be available to the user. */
         toolbar.add(checkPriceBtn);
         toolbar.add(addBtn);
         toolbar.add(removeBtn);
@@ -188,7 +245,10 @@ public class Main extends JFrame {
 
         return toolbar;
     }
-
+/**
+ * Here we create the main menu that will be displayed.
+ * @return the main menu
+ */
     private JMenu createMainMenu(){
         JMenu mainMenu = new JMenu("PriceWatcher");
         JMenuItem about, exit;
@@ -196,18 +256,25 @@ public class Main extends JFrame {
         mainMenu.setMnemonic(KeyEvent.VK_M);
         mainMenu.getAccessibleContext().setAccessibleDescription("Main Menu");
 
-        about = new JMenuItem("About", rescaleImage(createImageIcon("about.png")));
+        about = new JMenuItem("About", rescaleImage(createImageIcon("about.png"))); //adding the about icon png.
         about.setToolTipText("About PriceWatcher");
 
         exit = new JMenuItem("Quit");
 
         mainMenu.add(about);
-        mainMenu.add(exit);
+        mainMenu.add(exit); // closing the system when the user selects to Quit.
 
         return mainMenu;
 
     }
-
+/**
+ * This will create the Edit menu. Users will be able to see and select the options to:
+    * Check the price of the item
+    * Add a new item that the user wants to track the price of.
+    * Remove an item from the list that will no longer be tracked.
+    * Edit the information of an item that is currently being tracked.
+ * @return a menu that allows user to edit
+ */
     private JMenu createEditMenu(){
         JMenuItem checkPrices, addItem, removeItem, editItem;
         JMenu editMenu = new JMenu("Edit");
@@ -227,7 +294,7 @@ public class Main extends JFrame {
         editItem = new JMenuItem("Edit Item");
         editItem.setIcon(rescaleImage(createImageIcon("edit.png")));
         editItem.setToolTipText("Edit item");
-
+        /** Calling all methods that will allow buttons to perform their respective actions when clicked. */
         editMenu.add(checkPrices);
         editMenu.add(addItem);
         editMenu.add(removeItem);
@@ -236,25 +303,28 @@ public class Main extends JFrame {
 
         return editMenu;
     }
-
+/**
+ * Menu will allow the user to sort their items based on the price of the item.
+ * @return a menu that has been sorted.
+ */
     private JMenu createSortMenu(){
         JMenuItem priceChange;
         JMenu sortMenu = new JMenu("Sort");
 
         priceChange = new JMenuItem("Price Change (%)");
         priceChange.setToolTipText("Sort by price change");
-
-
-
         sortMenu.add(priceChange);
 
         return sortMenu;
 
     }
-
-    /** Create price update button. */
+/**
+ * Creating a button that will later be used to reflect the updated price of an item. 
+ * @return button to check price update.
+ * @see images folder for check icon
+ */
     private JButton createPriceUpdateButton() {
-        ImageIcon icon = rescaleImage(createImageIcon("check.png"));
+        ImageIcon icon = rescaleImage(createImageIcon("check.png"));    //adding the check icon
 
         JButton button = new JButton(icon);
         button.setFocusPainted(false);
@@ -263,20 +333,28 @@ public class Main extends JFrame {
 
         return button;
     }
-
-    /* Create View Page button */
+/**
+ * Creating a button that will later be used to open a browser and see the item through its URL.
+ * @return button to view the item in the browser.
+ * @see images folder for open link icon.
+ */
     private JButton createViewPageButton() {
         ImageIcon icon = rescaleImage(createImageIcon("openLink.png"));
 
         JButton button = new JButton(icon);
         button.setFocusPainted(false);
-        button.addActionListener(this::viewPageClicked);
+        button.addActionListener(this::viewPageClicked);    // checking that the button has been clicked to trigger another action to open browser and load URL.
         button.setToolTipText("Visit webpage");
 
         return button;
     }
 
-
+/**
+ * Here we simply resize the icons to look better in the JPanel.
+ * @param icon
+ * @return updated and resized icon.
+ * @see iamges folder for icons and their original sizes.
+ */
     private static ImageIcon rescaleImage(ImageIcon icon){
 
         Image rescaledImage = null;
@@ -289,8 +367,13 @@ public class Main extends JFrame {
         return icon;
     }
 
-
-    /* Create icon */
+/**
+ * Here we load our images onto the JPanel. When another method needs to load an image, this function will search for the image's name and load it onto the panel. Otherwise it will
+ * return an error because the file name was not in the images folder.
+ * @param filename
+ * @return
+ * @see images folder located in PriceWatcher/images/
+ */
     private ImageIcon createImageIcon(String filename) {
         URL imageUrl = getClass().getResource("../images/" + filename);
         if (imageUrl != null) {
@@ -301,8 +384,10 @@ public class Main extends JFrame {
             return null;
         }
     }
-
-    /** Show briefly the given string in the message bar. */
+/**
+ * This will load a quick message with the text that the user selected. This will last a total of 3 seconds.
+ * @param msg
+ */
     private void showMessage(String msg) {
         msgBar.setText(msg);
         new Thread(() -> {
@@ -316,20 +401,27 @@ public class Main extends JFrame {
         }).start();
     }
 
-
+/**
+ * Calling our main method.
+ * @param args
+ */
     public static void main(String[] args) {
         new Main();
     }
 
 }
+/**
+ * This will render each of the items of our list onto the panel
+ */
 
-/* Render each item from the list */
 class ItemListRenderer extends JLabel implements ListCellRenderer<Item>{
     DecimalFormat df = new DecimalFormat("###.##");
     public ItemListRenderer(){
         setOpaque(true);
     }
-
+/**
+ * Caling all our items and getting their information to display on the screen.
+ */
     @Override
     public Component getListCellRendererComponent(JList<? extends Item> list, Item item, int index, boolean isSelected, boolean cellHasFocus){
 
